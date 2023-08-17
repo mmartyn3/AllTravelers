8c25ea05aa72ae017bba67b1177d11cc4bcedfa2
Michael Martyn <michael.martyn.92@gmail.com>
2023-08-17 16:58:21 -0700

    Initial Commit

---
+++ AllTravelers/AllTravelersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllTravelersApp.swift
+//  AllTravelers
+//
+//  Created by Michael Martyn on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct AllTravelersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
